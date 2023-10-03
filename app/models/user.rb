715037a30c3b028ed4ff6a770d2fa63d0c801d1f
komashige <koma.shige0128@gmail.com>
2023-10-03 21:02:22 +0900

    新規登録　完了

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ApplicationRecord
   validates :last_name_kana, presence: true
   validates :birth_date, presence: true
 
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  VALID_KANJI_REGEX = /\A[一-龥々]+\z/.freeze
+  validates :first_name, :last_name, format: { with: VALID_KANJI_REGEX }
+
+  VALID_KANA_REGEX = /\A[ァ-ヶー－]+\z/.freeze
+  validates :first_name_kana, :last_name_kana, format: { with: VALID_KANA_REGEX }
+
 end
