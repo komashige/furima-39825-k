6ead077361e3994f4094ace3d6669aad7c7d143a
komashige <koma.shige0128@gmail.com>
2023-10-05 17:47:07 +0900

    テーブル、カラム作成

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -7,6 +7,16 @@ class FurimasController < ApplicationController
     @furima = Furima.new
   end
 
+  def create
+    Furima.create(furima_params)
+    redirect_to '/'
+  end
+
+  private
+  def furima_params
+    params.require(:furima).permit(:name, :category_id, :condition_id, :description, :user, :ship_cost_id, :ship_area_id, :ship_day_id, :price)
+  end
+
 end
 
  
