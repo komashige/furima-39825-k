a592ac4d18f514900853ed17947e95f2fd68d342
komashige <koma.shige0128@gmail.com>
2023-10-06 11:08:31 +0900

    ActiveHash　完了

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,13 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
   belongs_to :buy
   belongs_to :user
   has_one_attached :image
+
+  belongs_to_active_hash :category
+  belongs_to :condition
+  belongs_to :ship_cost
+  belongs_to :ship_area
+  belongs_to :ship_day
 end
