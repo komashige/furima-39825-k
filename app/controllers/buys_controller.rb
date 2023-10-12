c4cb2d28659f7b724a375948f605d4219aa770a1
komashige <koma.shige0128@gmail.com>
2023-10-12 15:48:23 +0900

    購入画面遷移　完了

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,6 +1,15 @@
 class BuysController < ApplicationController
-
+before_action :set_item, only: [:index, :create]
   def index
     @buy_form = BuyForm.new
   end
+
+  def create
+  end
+
+private
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
